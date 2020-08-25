e552cdd76e587d19b31f35d5e5219d53a6c62822
Monty Anderson <work@montyanderson.net>
2020-08-25 15:30:40 +0100

    support css modules

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,3 +3,9 @@
 		<h1>Hello, World!</h1>
 	</div>
 </template>
+
+<style>
+h1 {
+	font-family: sans-serif;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,30 @@ const { VueLoaderPlugin } = require('vue-loader');
 module.exports = {
   mode: 'development',
   entry: [
-    './src/index.js'
+	'./src/index.js'
   ],
   module: {
     rules: [
 		{
 			test: /\.vue$/,
 			use: 'vue-loader'
+		},
+		{
+			test: /\.css$/,
+			use: [
+				'vue-style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						// enable CSS Modules
+						modules: true
+					}
+				}
+			]
 		}
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+	  new VueLoaderPlugin()
   ]
 }
